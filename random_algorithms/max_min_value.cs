1afeec2b936f5faee7d4470399e08a0fa69f966c
Pharaoh <hamoncsl@hotmail.com>
2019-06-01 11:51:19 -0300

    Update and rename Program.cs to max_min_value.cs

--- random_algorithms/max_min_value.cs
+++ random_algorithms/max_min_value.cs
@@ -13,6 +13,7 @@ namespace "SEU NAMESPACE - O MESMO NOME DA SUA PASTA"
             Random rnd = new Random();
             int[] numbers = new int[10];
             int max = 0;
+            int min = 0;
             Console.Write("Os numeros da lista são: ");
             for (int i = 0; i < numbers.Length; i++)
             {
@@ -20,6 +21,7 @@ namespace "SEU NAMESPACE - O MESMO NOME DA SUA PASTA"
                 if (i == 0) 
                 {
                     max = numbers[i]; // Se o i for igual a zero, o primeiro valor, maximo é o primeiro valor.
+                    min = numbers[i];
                 }
                 if (numbers[i] > max)
                 {
@@ -27,11 +29,16 @@ namespace "SEU NAMESPACE - O MESMO NOME DA SUA PASTA"
                     // O maior numero agora é o index atual.
                     max = numbers[i]; 
                 }
+                if(numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
                 
                 Console.Write(numbers[i] + " ");
             }
             Console.WriteLine();
             Console.WriteLine("O maior valor da lista é: {0}", max);
+            Console.WriteLine("O menor valor da lista é: {0}", min);
             Console.ReadKey();
         }
     }
