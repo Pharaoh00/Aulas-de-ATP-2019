535a2858dabe367eb64a8aaa22deb3fea04ce3e5
Pharaoh <hamoncsl@hotmail.com>
2019-06-01 11:49:45 -0300

    Create Program.cs

---
+++ random_algorithms/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace "SEU NAMESPACE - O MESMO NOME DA SUA PASTA"
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numbers = new int[10];
+            int max = 0;
+            Console.Write("Os numeros da lista são: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(99); // Adicionar qualquer valor de 0 a 99 a lista numbers.
+                if (i == 0) 
+                {
+                    max = numbers[i]; // Se o i for igual a zero, o primeiro valor, maximo é o primeiro valor.
+                }
+                if (numbers[i] > max)
+                {
+                    // Se o index atual da lista numbers for maior que o max (maior numero?)
+                    // O maior numero agora é o index atual.
+                    max = numbers[i]; 
+                }
+                
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("O maior valor da lista é: {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
